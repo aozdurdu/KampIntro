fa03bc48c1611a01d3ff76eb5e035faf6e139d3f
Adil Özdurdu <aozdurdu@outlook.com>
2021-01-23 20:43:26 +0300

    HW_23-01-2021

---
+++ DictionaryIntro/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryIntro
+{
+    class MyDictionary<TKey, TValue> //for generic types
+    {
+        TKey[] keys;
+        TValue[] values;
+       
+        //constructor block
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeyArray = keys; //temporarily we referenced array items to tempKeyArray since in below we will use new function (wil be new reference) which it will delete old entries in the keys array 
+            keys = new TKey[keys.Length + 1]; //we should increase array element number as dynamic
+            
+            TValue[] tempValueArray = values; //temporarily we referenced array items to tempKeyArray since in below we will use new function (wil be new reference) which it will delete old entries in the keys array 
+            values = new TValue[values.Length + 1]; //we should increase array element number as dynamic
+            
+            //now we will handover old entries from tempKeyArray
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = tempValueArray[i];
+            }
+
+            keys[keys.Length - 1] = key; //Now we can add the new and last key in here
+            values[values.Length - 1] = value; //Now we can add the new and last key in here
+
+            Console.WriteLine("New Project Added: " + key + "-" + value);
+        }
+
+
+    }
+}
---
+++ DictionaryIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We will add new projects");
+
+            MyDictionary<int, string> myProjects = new MyDictionary<int, string>();
+            myProjects.Add(100, "Project1");
+            myProjects.Add(101, "Project2");
+            myProjects.Add(103, "Project3");
+            myProjects.Add(110, "Project6");
+            myProjects.Add(120, "Project80");
+
+            Console.WriteLine("--------");
+
+    }
+}
